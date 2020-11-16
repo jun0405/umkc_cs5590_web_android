e7aa19d2d96ead667c7bea7280cf60d8f0ac3a36
Saiaishwaryapuppala <66635678+Saiaishwaryapuppala@users.noreply.github.com>
2020-11-15 21:15:47 -0600

    Project

---
+++ WebProject2/WebProject2/WebProject2/app/src/main/java/com/example/webproject2/AffectedCountries.java
@@ -0,0 +1,26 @@
+package com.example.webproject2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.leo.simplearcloader.SimpleArcLoader;
+
+import org.json.JSONArray;
